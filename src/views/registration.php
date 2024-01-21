c515f0d17efb6a17ce4d41104ccb64c1268a95a9
Buricz9 <buricz1906@gmail.com>
2024-01-21 20:37:34 +0100

    Small changing in css

--- src/views/registration.php
+++ src/views/registration.php
@@ -4,13 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../public/css/registration.css">
+    <script type="text/javascript" src="../../public/js/registration.js" defer></script>
     <title>Registration</title>
 </head>
 <body>
 
 <div class="registration-container">
     <h2>Registration</h2>
-    <form>
+
+    <form id="registrationForm"> <!-- Added id to the form -->
         <label for="username">Nazwa użytkownika:</label>
         <input type="text" id="username" name="username" required>
 
@@ -29,7 +31,7 @@
             <span class="toggle-password" onclick="togglePasswordVisibility('confirm-password')">&#x1F441;</span>
         </div>
 
-        <button type="button" onclick="location.href='main'">Zarejestruj się</button>
+        <button type="submit">Zarejestruj się</button> <!-- Changed type to submit -->
     </form>
 </div>
 </body>
